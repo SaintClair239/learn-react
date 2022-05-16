0e73e67a8313638079fda77e44ddad44ca06fc8b
John <christophermoreno.jm2@gmail.com>
2022-05-16 08:29:38 +0800

    Added components

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-const ExpenseItem = ({date, title, amount}) => {
+const ExpenseItem = ({ date, title, amount }) => {
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <ExpenseDate date={date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
